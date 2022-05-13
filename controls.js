313d28af1c8128fa3fd383df2ae5c69a1b9452f2
ben.li <ben.li@rightcrowd.com>
2022-05-13 16:58:13 +1000

    first commit

---
+++ controls.js
@@ -0,0 +1,50 @@
+class Controls {
+  constructor() {
+    this.forward = false
+    this.left = false
+    this.right = false
+    this.reverse = false
+
+    this.#addKeyboardListeners()
+  }
+
+  //private method with #
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      // need arraw function to refer this=> one level above
+      // without arraw function, this => current function
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.left = true
+          break
+        case 'ArrowRight':
+          this.right = true
+          break
+        case 'ArrowUp':
+          this.forward = true
+          break
+        case 'ArrowDown':
+          this.reverse = true
+          break
+      }
+      console.table(this)
+    }
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.left = false
+          break
+        case 'ArrowRight':
+          this.right = false
+          break
+        case 'ArrowUp':
+          this.forward = false
+          break
+        case 'ArrowDown':
+          this.reverse = false
+          break
+      }
+      console.table(this)
+    }
+  }
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('myCanvas')
+canvas.width = 200
+
+const ctx = canvas.getContext('2d')
+const car = new Car(100, 100, 30, 50)
+car.draw(ctx)
+
+animate()
+
+function animate() {
+  car.update()
+
+  // resize hight also refresh the canvas
+  canvas.height = window.innerHeight
+  car.draw(ctx)
+
+  // calle animation in while loop, give illusion of movements
+  requestAnimationFrame(animate)
+}
