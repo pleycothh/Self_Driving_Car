69f9c68e7a8eee4648dc11bb88267a5472a2bc5c
ben.li <ben.li@rightcrowd.com>
2022-05-19 16:55:12 +1000

    try to add lerp function

--- controls.js
+++ controls.js
@@ -27,7 +27,6 @@ class Controls {
           this.reverse = true
           break
       }
-      console.table(this)
     }
     document.onkeyup = (event) => {
       switch (event.key) {
@@ -44,7 +43,6 @@ class Controls {
           this.reverse = false
           break
       }
-      console.table(this)
     }
   }
 }
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('myCanvas')
 canvas.width = 200
 
 const ctx = canvas.getContext('2d')
+const road = new Road(canvas.width / 2, canvas.width * 0.9)
 const car = new Car(100, 100, 30, 50)
 car.draw(ctx)
 
@@ -12,6 +13,8 @@ function animate() {
 
   // resize hight also refresh the canvas
   canvas.height = window.innerHeight
+
+  road.draw(ctx)
   car.draw(ctx)
 
   // calle animation in while loop, give illusion of movements
