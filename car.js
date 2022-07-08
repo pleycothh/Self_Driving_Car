cd303629e62756a5aa33f15607829e6dd31d1cee
ben.li <ben.li@rightcrowd.com>
2022-07-08 16:38:28 +1000

    had error at draw rays functons

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
     this.friction = 0.05
     this.angle = 0
 
+    this.sensor = new Sensor(this) // pass the car to sensor
     this.controls = new Controls()
   }
 
   update() {
     this.#move()
+    this.sensor.update()
   }
 
   #move() {
@@ -69,5 +71,7 @@ class Car {
     ctx.fill()
 
     ctx.restore()
+
+    this.sensor.draw(ctx)
   }
 }
---
+++ sensor.js
@@ -0,0 +1,42 @@
+class Sensor {
+  constructor(car) {
+    this.car = car
+    this.rayCount = 3
+    this.rayLength = 100
+    this.raySpread = Math.PI / 4
+    const und = undefined
+    this.rays = und || []
+  }
+
+  update() {
+    this.rays = []
+    for (let i = 0; i < this.rayCount; i++) {
+      const rayAngle = lerp(
+        this.raySpread / 2,
+        -this.raySpread / 2,
+        i / (this.rayCount - 1),
+      )
+
+      const start = { x: this.car.x, y: this.car.y }
+      const end = {
+        x: (this.car.x = Math.sin(rayAngle) * this.rayLength),
+        y: (this.car.y = Math.cos(rayAngle) * this.rayLength),
+      }
+
+      this.rays.push([start, end])
+    }
+  }
+
+  draw(ctx) {
+    const und = undefined
+    for (let i = 1; i < this.rayCount; i++) {
+      ctx.beginPath()
+      ctx.lineWidth = 2
+      ctx.strokeStyle = 'yellow'
+      console.log(this.rays[i][0])
+      // ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y)
+      // ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y)
+      ctx.stroke()
+    }
+  }
+}
