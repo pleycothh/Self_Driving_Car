64294beb534e01c2a353c68ff26773d4443271d6
Ben <ben.li19930119@gmail.com>
2022-07-10 15:31:39 +1000

    test

--- sensor.js
+++ sensor.js
@@ -1,16 +1,18 @@
 class Sensor {
   constructor(car) {
-    this.car = car
-    this.rayCount = 3
-    this.rayLength = 100
-    this.raySpread = Math.PI / 4
-    const und = undefined
-    this.rays = und || []
+    
+    this.car=car;
+    this.rayCount=5;
+    this.rayLength=150;
+    this.raySpread=Math.PI/2;
+
+    this.rays=[];
+    this.readings=[];
   }
 
   update() {
     this.rays = []
-    for (let i = 0; i < this.rayCount; i++) {
+    for (let i = 0; i < this.rayLength; i++) {
       const rayAngle = lerp(
         this.raySpread / 2,
         -this.raySpread / 2,
@@ -28,15 +30,21 @@ class Sensor {
   }
 
   draw(ctx) {
-    const und = undefined
-    for (let i = 1; i < this.rayCount; i++) {
-      ctx.beginPath()
-      ctx.lineWidth = 2
-      ctx.strokeStyle = 'yellow'
-      console.log(this.rays[i][0])
-      // ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y)
-      // ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y)
-      ctx.stroke()
+    //console.log(ctx);
+    for (let i = 0; i < this.rayCount; i++) {
+
+    //  if(this.rays[i] != undefined){
+       // console.log(this.rays[i])
+        let end=this.rays[i][1];
+  
+        ctx.beginPath()
+        ctx.lineWidth = 2
+        ctx.strokeStyle = 'yellow'
+        ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y)
+        ctx.lineTo(end.x, end.y)
+        ctx.stroke()
+   //   }
+
     }
   }
 }
